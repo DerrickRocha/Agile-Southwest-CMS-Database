87fd496e91b98d7b30dfb0f3f76735f37bbeb87a
derrick <drocha616@gmail.com>
2025-12-31 21:17:18 -0700

    Fixed sytax error.

--- database/migrations/0000_bootstrap.sql
+++ database/migrations/0000_bootstrap.sql
@@ -1,19 +1,23 @@
 SET XACT_ABORT ON;
 BEGIN TRAN;
 
-IF NOT EXISTS(SELECT 1 FROM sys.schemas WHERE name = 'app')
-BEGIN 
-    CREATE SCHEMA app;
-END 
+IF NOT EXISTS (SELECT 1
+               FROM sys.schemas
+               WHERE name = 'app')
+    BEGIN
+        CREATE SCHEMA app;
+    END
 
-IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'SchemaMigrations')
-BEGIN
-    CREATE TABLE app.SchemaMigrations (
-        MigrationId NVARCHAR(150) PRIMARY KEY,
-        AppliedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
-        AppliedBy NVARCHAR(128) NOT NULL,
-        Description NVARCHAR(500)
-    );
-END
+IF OBJECT_ID('app.SchemaMigrations', 'U') IS NULL
+    BEGIN
+        CREATE TABLE app.SchemaMigrations
+        (
+            MigrationId NVARCHAR(150) PRIMARY KEY,
+            AppliedAt   DATETIME2     NOT NULL DEFAULT SYSDATETIME(),
+            AppliedBy   NVARCHAR(128) NOT NULL,
+            Description NVARCHAR(500)
+        );
+    END
 
 COMMIT;
+
