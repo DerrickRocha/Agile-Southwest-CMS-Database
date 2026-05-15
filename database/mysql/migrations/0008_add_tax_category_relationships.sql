6e2a584c50865764427d39d4912c56387b21a763
derrick <drocha616@gmail.com>
2026-05-15 17:00:46 -0600

    added deleted at.

--- database/mysql/migrations/0008_add_tax_category_relationships.sql
+++ database/mysql/migrations/0008_add_tax_category_relationships.sql
@@ -2,6 +2,9 @@ START TRANSACTION;
 
 ALTER TABLE tax_categories ADD COLUMN tenant_id INT NOT NULL;
 ALTER TABLE tax_categories ADD CONSTRAINT tax_category_tenant_fk FOREIGN KEY(tenant_id) REFERENCES tenants(id);
+ALTER TABLE tax_categories ADD COLUMN deleted_at DATETIME NULL;
+
+CREATE INDEX idx_tax_categories_tenant_id ON tax_categories(tenant_id);
 
 INSERT INTO schema_migrations (migration_id,
                                applied_at,
