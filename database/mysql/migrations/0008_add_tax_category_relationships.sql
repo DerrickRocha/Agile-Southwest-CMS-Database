951678325e97ed9c5584d205cc6b22f35000e9d2
derrick <drocha616@gmail.com>
2026-05-15 16:55:58 -0600

    fixed syntax error.

--- database/mysql/migrations/0008_add_tax_category_relationships.sql
+++ database/mysql/migrations/0008_add_tax_category_relationships.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 
 ALTER TABLE tax_categories ADD COLUMN tenant_id INT NOT NULL;
-ALTER TABLE tax_categories ADD CONSTRAINT tax_category_tenant_fk(tenant_id) REFERENCES tenants(id);
+ALTER TABLE tax_categories ADD CONSTRAINT tax_category_tenant_fk FOREIGN KEY(tenant_id) REFERENCES tenants(id);
 
 INSERT INTO schema_migrations (migration_id,
                                applied_at,
